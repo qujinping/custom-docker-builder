4cfc49074d2bf50948cbd3066381eabc72f15837
qujinping <qujinping@qujinpingdeMacBook-Pro.local>
2017-04-13 14:20:58 +0800

    Add custom docker builder for building s2i base image

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# This image expects a set of environment variables to parameterize the build:
+#
+#   OUTPUT_REGISTRY - the Docker registry URL to push this image to
+#   VERSION - the version of image to be built
+#   BASE_IMAGE_NAME - the name of the base image 
+#   OPENSHIFT_NAMESPACES - the reposistory name of the image to be built
+#   SOURCE_REPOSITORY - a URI to fetch the build context from
+#   SOURCE_REF - a reference to pass to Git for which commit to use (optional)
+#
+# This image expects to have the Docker socket bind-mounted into the container.
+# If "/root/.dockercfg" is bind mounted in, it will use that as authorization
+# to a Docker registry.
+#
+FROM core/centos:7.3
+
+MAINTAINER qujinping
+
+RUN rpmkeys --import file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-7 && \
+    yum install -y epel-release && \
+    yum install -y --setopt=tsflags=nodocs gettext docker automake make python-setuptools python-pip git && \
+    yum clean all && \
+    (curl -L https://github.com/openshift/source-to-image/releases/download/v1.1.5/source-to-image-v1.1.5-4dd7721-linux-386.tar.gz | \
+         tar -xz -C /usr/local/bin ) 
+
+LABEL io.k8s.display-name="OpenShift Origin Custom Builder" \
+      io.k8s.description="This is a custom builder for use to build s2i images."
+ENV HOME=/root
+COPY build.sh /tmp/build.sh
+CMD ["/tmp/build.sh"]
---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+set -e -o pipefail
+IFS=$'\n\t'
+
+DOCKER_SOCKET=/var/run/docker.sock
+
+if [ ! -e "${DOCKER_SOCKET}" ]; then
+  echo "Docker socket missing at ${DOCKER_SOCKET}"
+  exit 1
+fi
+
+if [ -n "${VERSION}" ]; then
+  OUTPUT_IMAGE="${NAMESPACE}${BASE_IMAGE_NAME}-${VERSION//./}-centos7"
+else
+  OUTPUT_IMAGE="${NAMESPACE}${BASE_IMAGE_NAME}-centos7"
+fi
+
+if [ -n "${OUTPUT_IMAGE}" ]; then
+  TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+fi
+
+if [[ "${SOURCE_REPOSITORY}" != "git://"* ]] && [[ "${SOURCE_REPOSITORY}" != "git@"* ]]; then
+  URL="${SOURCE_REPOSITORY}"
+  if [[ "${URL}" != "http://"* ]] && [[ "${URL}" != "https://"* ]]; then
+    URL="https://${URL}"
+  fi
+  curl --head --silent --fail --location --max-time 16 $URL > /dev/null
+  if [ $? != 0 ]; then
+    echo "Could not access source url: ${SOURCE_REPOSITORY}"
+    exit 1
+  fi
+fi
+
+BUILD_DIR=$(mktemp --directory)
+git clone --recursive "${SOURCE_REPOSITORY}" "${BUILD_DIR}"
+if [ $? != 0 ]; then
+  echo "Error trying to fetch git source: ${SOURCE_REPOSITORY}"
+  exit 1
+fi
+if [ -n "${SOURCE_REF}" ]; then
+  pushd "${BUILD_DIR}"
+  git checkout "${SOURCE_REF}"
+  if [ $? != 0 ]; then
+    echo "Error trying to checkout branch: ${SOURCE_REF}"
+    exit 1
+  fi
+  popd
+fi
+
+if [[ -d /var/run/secrets/openshift.io/pull ]] && [[ ! -e /root/.dockercfg ]]; then
+  cp /var/run/secrets/openshift.io/pull/.dockercfg /root/.dockercfg
+fi
+
+pushd "${BUILD_DIR}"
+make build NAMESPACE=${NAMESPACE} VERSION=${VERSION} BASE_IMAGE_NAME=${BASE_IMAGE_NAME} 
+popd
+
+if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
+  cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
+fi
+
+if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
+  docker tag "${OUTPUT_IMAGE}" "${TAG}" 
+  docker push "${TAG}"
+fi
